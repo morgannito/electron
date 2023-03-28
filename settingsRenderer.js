2aba15ceb112452d82d568230d4fbdb7577c8c84
TheTreeSee <thetreesee@gmail.com>
2023-03-28 17:03:41 +0200

    change settings

--- settingsRenderer.js
+++ settingsRenderer.js
@@ -17,3 +17,23 @@ homeLink.addEventListener('click', (event) => {
   event.preventDefault();
   ipcRenderer.send('change-page', 'home');
 });
+
+
+const saveChangesButton = document.createElement('button');
+saveChangesButton.textContent = 'Save Changes';
+saveChangesButton.addEventListener('click', () => {
+  const confirmed = confirm('Are you sure you want to save changes?');
+  if (confirmed) {
+    const locationInput = document.getElementById('location');
+    const frequencySelect = document.getElementById('frequency');
+    const timeInput = document.getElementById('time');
+    const newSettings = {
+      location: locationInput.value,
+      frequency: frequencySelect.value,
+      time: timeInput.value
+    };
+    // save changes to JSON file using IPC message
+    ipcRenderer.send('save-settings', newSettings);
+  }
+});
+document.querySelector('main').appendChild(saveChangesButton);
